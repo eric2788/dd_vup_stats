d93504996e052810c86eaff4a9771c39218cbfc9
eric2788 <tsukiko852@gmail.com>
2022-04-14 14:37:07 +0800

    fix redis_test

--- service/db/redis_test.go
+++ service/db/redis_test.go
@@ -3,22 +3,7 @@ package db
 import (
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
-	"testing"
 )
-
-func aTestGetTakeIsVup(t *testing.T) {
-	err := PutUserIsVup(690608693, true)
-	if err != nil {
-		t.Fatal(err)
-	}
-	isVup, ok := GetUserIsVup(690608693)
-	if !ok {
-		t.Fatal(err)
-	}
-	if isVup != true {
-		t.Fatal("isVup is not true")
-	}
-}
 
 func ainit() {
 	logrus.SetLevel(logrus.DebugLevel)
