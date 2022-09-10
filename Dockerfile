6fb96907d088c47f3e8cb11170148454e9ac4bff
eric2788 <tsukiko852@gmail.com>
2022-09-10 22:31:12 +0800

    support pgsql database type, added DB_TYPE variable to support multiple rdbms

--- Dockerfile
+++ Dockerfile
@@ -16,11 +16,21 @@ COPY --from=builder /go/bin/dd_vup_stats /dd_vup_stats
 RUN chmod +x /dd_vup_stats
 
 ENV GIN_MODE=release
-ENV MYSQL_HOST=192.168.0.127
+
+ENV DB_TYPE=mysql
+
+ENV MYSQL_HOST=172.17.0.1
 ENV MYSQL_PORT=3306
 ENV MYSQL_USER=ddstats
 ENV MYSQL_PASS=changeme
 ENV MYSQL_DB=ddstats
+
+ENV PG_HOST=172.17.0.1
+ENV PG_PORT=5432
+ENV PG_USER=postgres
+ENV PG_PASS=changeme
+ENV PG_DB=ddstats
+
 ENV WEBSOCKET_URL=ws://192.168.0.127:8888/ws/global
 ENV BILIGO_HOST=http://192.168.0.127:8888
 ENV DEV_HOST=http://dev.example.com
--- service/db/database.go
+++ service/db/database.go
@@ -7,6 +7,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"os"
+	"strings"
 )
 
 var (
@@ -14,17 +15,14 @@ var (
 	Database *gorm.DB
 )
 
-func InitDB() {
-
-	log.Info("正在連接資料庫...")
-
+func getMySQLDataSource() string {
 	mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlPass := os.Getenv("MYSQL_PASS")
 	mysqlHost := os.Getenv("MYSQL_HOST")
 	mysqlPort := os.Getenv("MYSQL_PORT")
 	mysqlDB := os.Getenv("MYSQL_DB")
 
-	dataSource := fmt.Sprintf(
+	return fmt.Sprintf(
 		"%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=true",
 		mysqlUser,
 		mysqlPass,
@@ -32,6 +30,41 @@ func InitDB() {
 		mysqlPort,
 		mysqlDB,
 	)
+}
+
+func getPgSQLDataSource() string {
+	pgUser := os.Getenv("PG_USER")
+	pgPass := os.Getenv("PG_PASS")
+	pgHost := os.Getenv("PG_HOST")
+	pgPort := os.Getenv("PG_PORT")
+	pgDB := os.Getenv("PG_DB")
+
+	return fmt.Sprintf(
+		"host=%v port=%v user=%v dbname=%v password=%v sslmode=disable",
+		pgHost,
+		pgPort,
+		pgUser,
+		pgDB,
+		pgPass,
+	)
+}
+
+func InitDB() {
+
+	log.Info("正在連接資料庫...")
+
+	dbType := strings.ToLower(os.Getenv("DB_TYPE"))
+
+	var dataSource string
+
+	switch dbType {
+	case "mysql":
+		dataSource = getMySQLDataSource()
+	case "postgres":
+		dataSource = getPgSQLDataSource()
+	default:
+		log.Fatalf("不支持的資料庫類型: %v", dbType)
+	}
 
 	var logLevel logger.LogLevel
 
